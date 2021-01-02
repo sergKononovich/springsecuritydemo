694101a19676728b884a012d668168b768c3f268
sergeykon <beergood6@gmail.com>
2021-01-03 00:09:53 +0300

    Использование аннотации @PreAuthorize. Можно использовать, дабы не раздувать конфиги, в случае если у нас много эндпоинтов и на каждый нужны свои разрешения(permissions)

--- src/main/java/com/konon/hobby/springsecuritydemo/rest/DeveloperRestController.java
+++ src/main/java/com/konon/hobby/springsecuritydemo/rest/DeveloperRestController.java
@@ -1,6 +1,7 @@
 package com.konon.hobby.springsecuritydemo.rest;
 
 import com.konon.hobby.springsecuritydemo.model.Developer;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -17,11 +18,13 @@ public class  DeveloperRestController {
     ));
 
     @GetMapping
+    @PreAuthorize("hasAuthority('developers:read')")
     public List<Developer> getAll() {
         return developers;
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('developers:read')")
     public Developer getById(@PathVariable Long id) {
         return developers.stream().filter(developer -> developer.getId().equals(id))
                 .findFirst()
@@ -29,12 +32,14 @@ public class  DeveloperRestController {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('developers:write')")
     public Developer create(@RequestBody Developer developer) {
         this.developers.add(developer);
         return developer;
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('developers:write')")
     public void deleteById(@PathVariable Long id) {
         this.developers.removeIf(developer -> developer.getId().equals(id));
     }

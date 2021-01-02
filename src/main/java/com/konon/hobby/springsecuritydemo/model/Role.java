7126ae70dff9fd021b09b94e34d688af6603b189
sergeykon <beergood6@gmail.com>
2021-01-02 23:50:40 +0300

    Доступ к сервису на основе permissions(разрешений) вместо ролей

--- src/main/java/com/konon/hobby/springsecuritydemo/model/Role.java
+++ src/main/java/com/konon/hobby/springsecuritydemo/model/Role.java
@@ -1,6 +1,27 @@
 package com.konon.hobby.springsecuritydemo.model;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public enum Role {
-    ADMIN,
-    USER
+    ADMIN(Set.of(Permission.DEVELOPERS_READ, Permission.DEVELOPERS_WRITE)),
+    USER(Set.of(Permission.DEVELOPERS_READ));
+
+    private final Set<Permission> permissions;
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getAuthorities() {
+        return getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+    }
 }
